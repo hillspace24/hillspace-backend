901ffe0b329e558e60fd1828d4c30152b94b43c3
Ephraim7223 <basseyephraim55@gmail.com>
2026-07-19 00:32:29 +0100

    fixed cors issues

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
